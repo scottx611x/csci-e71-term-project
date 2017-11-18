4e728c0ee73083dacc1f10cd03785baa24429a3d
Scott Ouellette <scottx611x@gmail.com>
2017-11-17 23:31:15 -0500

    Cleanup Asset

--- app-context/app/Asset.php
+++ app-context/app/Asset.php
@@ -7,23 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Asset extends Model
 {
     protected $table = 'assets';
-
-    // public $owner;
-
-    // public $purchase_price;
-
-    // public $purchase_date;
-
-    // public $serial_number;
-
-    // public $estimated_life_months;
-
-    // public $assigned_to;
-
-    // public $assigned_date;
-
-    // public $tag;
-
-    // public $scheduled_retirement_year;
-
 }
