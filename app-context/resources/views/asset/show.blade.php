e150b2439528d05a9067a24e0285c9219398aae0
Scott Ouellette <scottx611x@gmail.com>
2017-11-17 23:57:58 -0500

    Tiday display code, add links to detail views

--- app-context/resources/views/asset/show.blade.php
+++ app-context/resources/views/asset/show.blade.php
@@ -10,7 +10,7 @@ View Asset
     <div class="container">
         <div class="row justify-content-center">
             @if (count($result)===1)
-                <div class="title col-12 col-lg-8">
+                <div class="title col-12 col-lg-12 col-xl-12">
                     <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">
                         View Asset
                     </h2>
@@ -29,7 +29,7 @@ View Asset
                 </div>
             @endif
             @if (count($result)>1)
-                <div class="title col-12 col-lg-12">
+                <div class="title col-12 col-lg-12 col-xl-12">
                     <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">
                         View Assets
                     </h2>
@@ -46,7 +46,11 @@ View Asset
                                 @foreach ($result as $rec)
                                     <tr>
                                         @foreach($rec->toArray() as $key => $value)
-                                            <td>{{ $value }}</td>
+                                            @if ($key=='id')
+                                                <td><a href="./{{$value}}">{{$value}}</a></td>
+                                            @else
+                                                <td>{{ $value }}</td>
+                                            @endif
                                         @endforeach
                                     </tr>
                                 @endforeach
