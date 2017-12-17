95bebb1fa21c417c750c6391d191324f90792f9e
Scott Ouellette <scottx611x@gmail.com>
2017-12-17 17:42:15 -0500

    Update

--- app-context/install_deps.sh
+++ app-context/install_deps.sh
@@ -4,5 +4,5 @@ curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
 php /usr/local/bin/composer install --no-interaction --prefer-source --no-dev
 php artisan key:generate
 php artisan optimize
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' database:33061)" != "200" ]]; do sleep 5; done'
 php artisan migrate
+php artisan serve --port=8000
