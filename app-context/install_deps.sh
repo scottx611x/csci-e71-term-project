ed6356d6eb05a33bc537bf9ba06695dbf918e6ed
Scott Ouellette <scottx611x@gmail.com>
2017-12-17 12:34:38 -0500

    No dev reqs

--- app-context/install_deps.sh
+++ app-context/install_deps.sh
@@ -1,7 +1,7 @@
 cp .env.prod .env
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-php /usr/local/bin/composer install --no-interaction --prefer-source
+php /usr/local/bin/composer install --no-interaction --prefer-source --no-dev
 php artisan key:generate
 php artisan optimize
 php artisan migrate
