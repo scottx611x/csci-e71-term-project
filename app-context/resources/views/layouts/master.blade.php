5886995bef98f07d11f210af62b6204ad242a8f1
Scott Ouellette <scottx611x@gmail.com>
2017-12-17 22:52:43 -0500

    Remove html & css validator links

--- app-context/resources/views/layouts/master.blade.php
+++ app-context/resources/views/layouts/master.blade.php
@@ -115,14 +115,6 @@
 	<section>
 		@stack('body')
 	</section>
-
-	<footer class="text-center">
-		<br />
-		<p>
-			<a href="http://validator.w3.org/check/referer" target="_blank"><img style="border:0;width:88px;height:31px" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" /></a>
-			<a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
-		</p>
-	</footer>
 </body>
 @stack('scripts')
 </html>
